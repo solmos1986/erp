42e41d481f83c290fafef9a326b2999d32c42edf
stiven lovera <stivenlovera@gmail.com>
2024-03-04 23:53:38 -0400

    cambio para master

--- resources/views/layouts/parts/sidebar.blade.php
+++ resources/views/layouts/parts/sidebar.blade.php
@@ -1,20 +1,14 @@
 <div class="app-menu">
-    <!-- Brand Logo -->
     <div class="logo-box">
-        <!-- Brand Logo Light -->
         <a href="index.html" class="logo-light">
             <img src="{{ asset('/images/logo-light.png') }}" alt="logo" class="logo-lg">
             <img src="{{ asset('/images/logo-sm.png') }}" alt="small logo" class="logo-sm">
         </a>
-
-        <!-- Brand Logo Dark -->
         <a href="index.html" class="logo-dark">
             <img src="{{ asset('/images/logo-dark.png') }}" alt="dark logo" class="logo-lg">
             <img src="{{ asset('/images/logo-sm.png') }}" alt="small logo" class="logo-sm">
         </a>
     </div>
-
-    <!-- menu-left -->
     <div class="scrollbar">
         <!-- User box -->
         <div class="user-box text-center">
